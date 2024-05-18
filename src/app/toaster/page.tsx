6a089357941732b59704ebd1a1debcd2b81f5f5f
Kunal <kunalpaul673@gmail.com>
2024-05-18 22:05:36 +0530

    commit on toaster

--- src/app/toaster/page.tsx
+++ src/app/toaster/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-function Toaster({message,icon}:{message:string,icon:string}) {
+function Toaster({message,icon}:any) {
   return (
     <div className="w-full  flex flex-row justify-center items-center p-3 transition ease-in delay-200 duration-1000">
       <div className="w-full md:w-2/5 h-40 flex  flex-col  justify-center items-center rounded-md dark:bg-gradient-to-r dark:from-gray-700 dark:via-gray-900 dark:to-black bg-gradient-to-r from-white via-gray-600 to-black dark:text-gray-900 bg-gray-900 text-white">
