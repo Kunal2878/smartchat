6fdfea5caba1fc7dfd5d30d4a88c396d0b5b2d21
Kunal <kunalpaul673@gmail.com>
2024-05-31 13:01:20 +0530

    server modified

--- src/app/api/socket_server/route.ts
+++ src/app/api/socket_server/route.ts
@@ -7,7 +7,7 @@ import { cookies } from 'next/headers'
 import { Server, ServerOptions } from 'socket.io';
 import { NextApiRequest, NextApiResponse } from 'next';
 import Chat_msg from "../socket_client/client"
-const Chat_server=(req:any,res:any)=> {
+const GET=(req:any,res:any)=> {
 
   // if (!res.socket?.server.io) {
     console.log('*First use, starting socket.io');
@@ -45,4 +45,4 @@ const Chat_server=(req:any,res:any)=> {
 //     <div className='w-full '> <Chat_msg/></div>
 //   )
 // }
-export default Chat_server
+export default GET
