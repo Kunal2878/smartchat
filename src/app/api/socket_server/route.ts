6d30686d16e3414adc995c73b74ac51cb1aef552
Kunal <kunalpaul673@gmail.com>
2024-05-31 13:07:57 +0530

    server

--- src/app/api/socket_server/route.ts
+++ src/app/api/socket_server/route.ts
@@ -7,17 +7,18 @@ import { cookies } from 'next/headers'
 import { Server, ServerOptions } from 'socket.io';
 import { NextApiRequest, NextApiResponse } from 'next';
 import Chat_msg from "../socket_client/client"
-const GET=(req:any,res:any)=> {
-
-  // if (!res.socket?.server.io) {
+export async function GET(res: any) {
+  if (!res.socket?.server.io) {
     console.log('*First use, starting socket.io');
 
-    // const io = new Server<ServerOptions>(res.socket.server); // Allow options
-    const io = new Server(res.socket.server,{path:'/api/socket_server',cors:{
-      origin:'*',
-      methods:['GET', 'POST','PUT']
-     }},
-  );
+    const io = new Server(res.socket.server, {
+      path: '/api/socket_server',
+      cors: {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT']
+      }
+    });
+
     io.on('connection', (socket) => {
       socket.broadcast.emit('a user connected');
       socket.on('hello', (msg: string) => {
@@ -26,23 +27,13 @@ const GET=(req:any,res:any)=> {
     });
 
     res.socket.server.io = io;
-  // } else {
-  //   console.log('socket.io already running');
-  // }
-
-  // res.end();
-
-
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
+  } else {
+    console.log('socket.io already running');
   }
+}
 //   export default function Show_chat(){
 //     // const h= Handler();
 //   return(
 //     <div className='w-full '> <Chat_msg/></div>
 //   )
 // }
-export default GET
