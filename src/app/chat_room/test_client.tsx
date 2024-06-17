399c2ca4be8ad34f683197ddd2bd8d331a74b9df
Kunal <kunalpaul673@gmail.com>
2024-06-17 17:04:46 +0530

    changed

--- src/app/chat_room/test_client.tsx
+++ src/app/chat_room/test_client.tsx
@@ -160,7 +160,7 @@ isThemeMenu&&(
 
 
 <div className='  w-full mb-20 md:mb-0 pb-26 H-con  flex flex-col'   style={{zIndex:-10}}>
-<div className=" hidden md:flex absolute top-0 w-full  flex-row justify-center items-center h-10 p-2 rounded-md text-white text-[10px] "> <div className=' p-2 rounded-md w-1/5 dark:bg-gradient-to-tr dark:from-indigo-900 dark:via-gray-800 dark:to-gray-900 bg-gradient-to-tr from-purple-400 via-purple-100 to-gray-600 dark:text-white flex flex-row justify-between items-center'> <span>{testName}</span>  <span> Chat id: {testRoom}</span> </div> </div>
+<div className=" hidden md:flex absolute top-0 w-full  flex-row justify-center items-center h-10 p-2 rounded-md text-white text-[10px] "> <div className=' p-2 rounded-md w-1/5 bg-gradient-to-tr from-indigo-900 dark:via-gray-800to-gray-900  text-yellow-400 flex flex-row justify-center items-center'> <span className='mr-3'>{testName}</span>  <span> Chat id: {testRoom}</span> </div> </div>
 
 <div className='w-full flex flex-col mt-8'>
 
